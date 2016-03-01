aead7a63967a364a140e2a5f0e7c0ba92b6dd51d
halomademeapc <opr9029@gmail.com>
2016-02-29 19:51:12 -0500

    metropolis mayhem badge

---
+++ assets/col_metromayhem/col_metromayhem_edge.js
@@ -0,0 +1,53 @@
+
+(function(compId){var _=null,y=true,n=false,x46='87',e6='${Sensor}',x18='rgba(0,6,18,1.00)',x51='newgrounds',x31='136px',e7='${hovercycle}',x38='Text2',x5='rgba(0,13,36,1.00)',x36='rgba(255,255,255,1.00)',i='none',x3='6.0.0.400',x33='281px',x22='90px',x27='mayhemprev',x15='inset',x16='rgba(0,0,0,0.65098)',x12='auto',x11='270px',x37='400',xc='rgba(0,0,0,1)',x4='rgba(0,13,36,0)',x1='6.0.0',x2='5.0.0',x8='metro',x35='37px',x17='Rectangle',x21='462px',po='center',x19='9px',x49='true',x30='237px',x='text',x45='439',x10='480px',m='rect',x9='0px',x40='540px',e54='${newgrounds}',p='px',x55='youtube',zy='scaleY',e57='${youtube}',zx='scaleX',x20='295px',x52='1.14999',x13='rgba(0,0,0,0)',x41='rgb(0, 0, 0)',x25='Arial, Helvetica, sans-serif',x47='yt_but',l='normal',g='image',x50='87px',b='block',x26='break-word',x23='Text',tp='top',x48='384',x29='390px',d='display',x44='281',x42='rgba(0,13,36,0.00)',x28='21px',x34='190px',x43='ng_button';var g56='youtube.svg',g32='mayhemprev.jpg',g53='newgrounds.svg',g14='metro.png';var s24="<p style=\"margin: 0px; text-align: center;\">​<span style=\"background-color: rgba(255, 255, 255, 0); color: rgb(255, 255, 255); font-size: 18px;\">Every year the Sonic Paradox crew has an animation for the Summer of Sonic convention.&nbsp;\n</span></p><p style=\"margin: 0px; text-align: center;\"><span style=\"background-color: rgba(255, 255, 255, 0); color: rgb(255, 255, 255); font-size: 18px;\">This one was Metropolis Mayhem!</span></p>",s39="<p style=\"margin: 0px;\">​<span style=\"font-family: changa-one, sans-serif;\">WATCH ON</span></p>";var im='images/',aud='media/',vid='media/',js='js/',fonts={'changa-one, sans-serif':'<script src=\"http://use.edgefonts.net/changa-one:n4,i4:all.js\"></script>'},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:'hovercycle',symbolName:'hovercycle',t:m,r:['0','0px','480','540','auto','auto']},{id:'Ltrigger',t:m,r:['-9px','0px','13px','270px','auto','auto'],f:[x4],s:[0,"rgb(0, 0, 0)",i]},{id:'Btrigger',t:m,r:['0px','268px','480px','12px','auto','auto'],f:[x4],s:[0,"rgb(0, 0, 0)",i]},{id:'Ttrigger',t:m,r:['0px','-9px','480px','12px','auto','auto'],f:[x4],s:[0,"rgb(0, 0, 0)",i]},{id:'Rtrigger',t:m,r:['477px','0px','13px','270px','auto','auto'],f:[x4],s:[0,"rgb(0, 0, 0)",i]},{id:'Sensor',v:b,t:m,r:['0px','0px','480px','270px','auto','auto'],f:[x4],s:[0,"rgb(0, 0, 0)",i]}],style:{'${Stage}':{isStage:true,r:[undefined,undefined,'480px','270px'],overflow:'hidden',f:[x5]}}},tt:{d:250,a:y,data:[["eid18",d,0,0,"easeInOutSine",e6,b,b],["eid19",d,19,0,"easeInOutSine",e6,b,i],["eid11",tp,0,250,"easeInOutSine",e7,'0px','-270px']]}},"hovercycle":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:x8,t:g,r:[x9,x9,x10,x11,x12,x12],f:[x13,im+g14,x9,x9]},{r:[x9,x11,x10,x11,x12,x12],boxShadow:[x15,0,13,13,0,x16],id:x17,s:[0,xc,i],t:m,f:[x18]},{r:[x19,x20,x21,x22,x12,x12],id:x23,text:s24,n:[x25,[24,''],xc,l,i,'',x26,l],t:x},{id:x27,t:g,r:[x28,x29,x30,x31,x12,x12],f:[x13,im+g32,x9,x9]},{r:[x33,x29,x34,x35,x12,x12],ts:['','','','',i],n:[x25,[28,p],x36,x37,i,l,x26,l],id:x38,text:s39,align:po,t:x},{r:[x9,x9,x10,x40,x12,x12],id:b,s:[0,x41,i],t:m,f:[x42]},{id:x43,sN:x43,r:[x44,x45,x46,x46,x12,x12],t:m},{id:x47,sN:x47,r:[x48,x45,x46,x46,x12,x12],t:m}],style:{'${symbolSelector}':{isStage:x49,r:[undefined,undefined,x10,x40]}}},tt:{d:125,a:y,data:[]}},"ng_button":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{r:[x9,x9,x50,x50,x12,x12],id:x51,tf:[[],[],[],[x52,x52]],t:g,f:[x13,im+g53,x9,x9]}],style:{'${symbolSelector}':{isStage:x49,r:[undefined,undefined,x50,x50]}}},tt:{d:125,a:y,data:[["eid22",zx,0,125,"easeInOutSine",e54,'1','1.14999'],["eid23",zy,0,125,"easeInOutSine",e54,'1','1.14999']]}},"yt_but":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:x55,t:g,r:[x9,x9,x50,x50,x12,x12],f:[x13,im+g56,x9,x9]}],style:{'${symbolSelector}':{isStage:x49,r:[undefined,undefined,x50,x50]}}},tt:{d:125,a:y,data:[["eid26",zx,0,125,"easeInOutSine",e57,'1','1.14999'],["eid27",zy,0,125,"easeInOutSine",e57,'1','1.14999']]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-189808421");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${Rtrigger}","mouseover",function(sym,e){sym.playReverse();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${Ttrigger}","mouseover",function(sym,e){sym.playReverse();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${Btrigger}","mouseover",function(sym,e){sym.playReverse();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${Ltrigger}","mouseover",function(sym,e){sym.playReverse();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${Sensor}","mouseover",function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'hovercycle'
+(function(symbolName){})("hovercycle");
+//Edge symbol end:'hovercycle'
+
+//=========================================================
+
+//Edge symbol: 'ng_button'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${newgrounds}","click",function(sym,e){window.open("http://www.newgrounds.com/portal/view/623819","_blank");});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${newgrounds}","mouseover",function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${newgrounds}","mouseout",function(sym,e){sym.playReverse();});
+//Edge binding end
+})("ng_button");
+//Edge symbol end:'ng_button'
+
+//=========================================================
+
+//Edge symbol: 'yt_but'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${youtube}","click",function(sym,e){window.open("https://www.youtube.com/watch?v=bSj7yRyPWaQ","_blank");});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${youtube}","mouseover",function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${youtube}","mouseout",function(sym,e){sym.playReverse();});
+//Edge binding end
+})("yt_but");
+//Edge symbol end:'yt_but'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-189808421");
